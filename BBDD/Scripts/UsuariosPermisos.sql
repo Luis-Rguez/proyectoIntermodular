351773dfc328677073ecb5e9ef7c012404629bb6
Luis-Rguez <albertorguezvera@gmail.com>
2026-04-11 18:22:31 +0100

    Grant additional SELECT privileges to usuario_ajedrez
    Restringir permisos de perfil usuario, solo se le permite ver las tablas de clasificacion, formato de torneo, partidas, juegan y movimientos

--- BBDD/Scripts/UsuariosPermisos.sql
+++ BBDD/Scripts/UsuariosPermisos.sql
@@ -2,4 +2,8 @@ CREATE USER 'admin_ajedrez'@'localhost' IDENTIFIED BY 'admin';
 CREATE USER 'usuario_ajedrez'@'%' IDENTIFIED BY 'usuario';
 
 GRANT ALL PRIVILEGES ON torneo_ajedrez.* TO 'admin_ajedrez';
-GRANT SELECT ON torneo_ajedrez.* TO 'usuario_ajedrez';
+GRANT SELECT ON torneo_ajedrez.clasificacion TO 'usuario_ajedrez';
+GRANT SELECT ON torneo_ajedrez.partidas TO 'usuario_ajedrez';
+GRANT SELECT ON torneo_ajedrez.formato_torneo TO 'usuario_ajedrez';
+GRANT SELECT ON torneo_ajedrez.movimientos TO 'usuario_ajedrez';
+GRANT SELECT ON torneo_ajedrez.juegan TO 'usuario_ajedrez';
