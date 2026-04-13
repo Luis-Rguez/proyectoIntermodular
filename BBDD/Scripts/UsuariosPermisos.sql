f6396ed6f1b397dd3a7249dcc8d1f3c4dddaf8d1
Luis-Rguez <albertorguezvera@gmail.com>
2026-04-13 17:14:13 +0100

    Add arbitros and permissions for arbitro_ajedrez
    Se crea un nuevo usuario llamado arbitro y se le concede permisos parciales.

--- BBDD/Scripts/UsuariosPermisos.sql
+++ BBDD/Scripts/UsuariosPermisos.sql
@@ -1,9 +1,15 @@
 CREATE USER 'admin_ajedrez'@'localhost' IDENTIFIED BY 'admin';
 CREATE USER 'usuario_ajedrez'@'%' IDENTIFIED BY 'usuario';
+CREATE USER 'arbitro_ajedrez'@'%' IDENTIFIED BY 'arbitro';
 
 GRANT ALL PRIVILEGES ON torneo_ajedrez.* TO 'admin_ajedrez';
-GRANT SELECT ON torneo_ajedrez.clasificacion TO 'usuario_ajedrez';
+
+GRANT SELECT ON torneo_ajedrez.* TO 'usuario_ajedrez';
 GRANT SELECT ON torneo_ajedrez.partidas TO 'usuario_ajedrez';
 GRANT SELECT ON torneo_ajedrez.formato_torneo TO 'usuario_ajedrez';
 GRANT SELECT ON torneo_ajedrez.movimientos TO 'usuario_ajedrez';
 GRANT SELECT ON torneo_ajedrez.juegan TO 'usuario_ajedrez';
+
+GRANT SELECT ON torneo_ajedrez.juegan TO 'arbitro_ajedrez';
+GRANT SELECT ON torneo_ajedrez.clasificacion TO 'arbitro_ajedrez';
+GRANT SELECT, INSERT, UPDATE ON torneo_ajedrez.movimientos TO 'arbitro_ajedrez';
